747165f8cdea3f967a6ba03a26b43fcac253bdec
Najam <najamiftikhar2020@gmail.com>
2020-12-25 20:32:15 +0500

    Navbar

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,15 @@
+import Jumbo from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
+import styled from 'styled-components'
+import boatImage from '../assets/boat.jpg'
+
+
+
+const Jumbotron = (props)=>{
+
+    return (
+
+    )
+}
+
+export default Jumbotron
