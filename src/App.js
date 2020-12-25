06fa392479d09dd5b9a3349beff3349ec21f0758
Najam <najamiftikhar2020@gmail.com>
2020-12-25 19:56:34 +0500

    routing and pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import NoMatch from "./NoMatch";
 
 function App() {
   return (
@@ -7,6 +11,9 @@ function App() {
       <Router>
         <Switch>
           <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+          <Route component={NoMatch} />
         </Switch>
       </Router>
     </React.Fragment>
---
+++ src/Contact.js
@@ -0,0 +1,57 @@
+const Contact = () => {
+  return (
+    <div>
+      <h2>Contact</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at
+        malesuada enim, at dictum dui. Quisque fringilla quam libero, non
+        tincidunt augue condimentum eget. Phasellus ac diam id nibh elementum
+        molestie. Phasellus malesuada vehicula eros ut convallis. Aenean quis
+        venenatis ante, eu fermentum nulla. Vestibulum ante ipsum primis in
+        faucibus orci luctus et ultrices posuere cubilia curae; Integer dapibus
+        massa sed lacinia dapibus. Praesent a convallis eros, nec maximus elit.
+        In gravida consectetur lacus eget commodo. Donec dapibus, lectus et
+        imperdiet finibus, massa metus feugiat nisl, sit amet pharetra enim enim
+        in sapien. Donec finibus nibh dui, at vehicula magna facilisis eget.
+        Proin suscipit facilisis dui vitae imperdiet. Quisque vel velit eu massa
+        dapibus eleifend eu id justo. Pellentesque sem neque, vestibulum sit
+        amet commodo nec, sollicitudin vulputate dolor. Nam maximus id elit nec
+        placerat.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at
+        malesuada enim, at dictum dui. Quisque fringilla quam libero, non
+        tincidunt augue condimentum eget. Phasellus ac diam id nibh elementum
+        molestie. Phasellus malesuada vehicula eros ut convallis. Aenean quis
+        venenatis ante, eu fermentum nulla. Vestibulum ante ipsum primis in
+        faucibus orci luctus et ultrices posuere cubilia curae; Integer dapibus
+        massa sed lacinia dapibus. Praesent a convallis eros, nec maximus elit.
+        In gravida consectetur lacus eget commodo. Donec dapibus, lectus et
+        imperdiet finibus, massa metus feugiat nisl, sit amet pharetra enim enim
+        in sapien. Donec finibus nibh dui, at vehicula magna facilisis eget.
+        Proin suscipit facilisis dui vitae imperdiet. Quisque vel velit eu massa
+        dapibus eleifend eu id justo. Pellentesque sem neque, vestibulum sit
+        amet commodo nec, sollicitudin vulputate dolor. Nam maximus id elit nec
+        placerat.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi at
+        malesuada enim, at dictum dui. Quisque fringilla quam libero, non
+        tincidunt augue condimentum eget. Phasellus ac diam id nibh elementum
+        molestie. Phasellus malesuada vehicula eros ut convallis. Aenean quis
+        venenatis ante, eu fermentum nulla. Vestibulum ante ipsum primis in
+        faucibus orci luctus et ultrices posuere cubilia curae; Integer dapibus
+        massa sed lacinia dapibus. Praesent a convallis eros, nec maximus elit.
+        In gravida consectetur lacus eget commodo. Donec dapibus, lectus et
+        imperdiet finibus, massa metus feugiat nisl, sit amet pharetra enim enim
+        in sapien. Donec finibus nibh dui, at vehicula magna facilisis eget.
+        Proin suscipit facilisis dui vitae imperdiet. Quisque vel velit eu massa
+        dapibus eleifend eu id justo. Pellentesque sem neque, vestibulum sit
+        amet commodo nec, sollicitudin vulputate dolor. Nam maximus id elit nec
+        placerat.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
